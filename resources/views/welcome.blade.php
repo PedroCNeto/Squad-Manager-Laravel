2bd465b597de22ccdffef8b96499470684ac500b
PedroCNeto <pedrocastelarineto@gmail.com>
2024-07-18 17:35:41 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<?php include_once(base_path('resources\Includes\Header.php')); ?>
+
+  </head>
+  <body>
+  <nav class="navbar" style="background-color: #0e1111">
+        <div class="container-fluid" >
+            <a class="navbar-brand ms-auto me-auto" style="color: white" href="#">
+                Squad Manager
+            </a>
+        </div>
+    </nav>
+    <h1>Hello, world!</h1>
+  </body>
+
+<?php include_once(base_path('resources\Includes\Footer.php')); ?>
