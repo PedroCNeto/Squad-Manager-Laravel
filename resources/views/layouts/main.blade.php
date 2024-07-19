54e4f16d7473003ed946b5f07a1b0deab059a344
PedroCNeto <pedrocastelarineto@gmail.com>
2024-07-19 14:29:38 -0300

    Corrigindo responsividade e tela principal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,17 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
 
   <body>
     <nav class="navbar" style="background-color: #0e1111">
         <div class="container-fluid" >
-            <a class="navbar-brand ms-auto me-auto" style="color: white" href="#">
+            <a class="navbar-brand ms-auto me-auto title-nav" style="color: white" href="/">
                 Squad Manager
             </a>
         </div>
     </nav>
+    
+    <nav class="navbar navbar-expand" style="background-color: #1e1e1e">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto me-auto">
+          <li class="nav-item active">
+            <a class="nav-link" style="color: white" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: white" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: white" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" style="color: white" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     @yield('content')
   </body>
 
