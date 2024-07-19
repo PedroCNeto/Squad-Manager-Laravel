010908b69c69bf12d56a9ac9978453eafe5c393f
PedroCNeto <pedrocastelarineto@gmail.com>
2024-07-18 22:39:53 -0300

    Aprendendo Section e Yield

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,8 @@
-@include('blade_includes.header') 
+@extends('layouts.main')
 
-  </head>
-  <body>
-  <nav class="navbar" style="background-color: #0e1111">
-        <div class="container-fluid" >
-            <a class="navbar-brand ms-auto me-auto" style="color: white" href="#">
-                Squad Manager
-            </a>
-        </div>
-    </nav>
-    <h1>Hello, world!</h1>
-  </body>
+@section('title', 'Squad Manager - Main')
 
-@include('blade_includes.footer') 
+@section('content')
+<h1>Hello, world!</h1>
+@endsection
 
