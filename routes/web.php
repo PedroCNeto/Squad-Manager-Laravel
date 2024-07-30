60270e6b06817bd12a02b77e12c97fbf0838895b
PedroCNeto <pedrocastelarineto@gmail.com>
2024-07-29 22:01:03 -0300

    Adicionando o Jetstream e o livewire

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ Route::get('/createmission', [MissionController::class, 'createmissionpage']);
 Route::post('/createmission', [MissionController::class, 'store']);
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
