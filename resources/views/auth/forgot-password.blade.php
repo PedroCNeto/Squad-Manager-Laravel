5bf36f8d351d91733534401f5bb69b897336545a
PedroCNeto <pedrocastelarineto@gmail.com>
2024-08-05 21:35:00 -0300

    Paginas de senha e registro personalizadas basico

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,13 +1,11 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('layouts.simple')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+@section('title', 'Register - Squad Manager')
 
+@section('content')
+
+<main>
+    <div class="d-flex justify-content-center flex-direction-column align-items-center" style="height: 100vh;">
         @session('status')
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ $value }}
@@ -15,20 +13,29 @@
         @endsession
 
         <x-validation-errors class="mb-4" />
-
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
+            <h1 class="text-center mb-4 font-weight-bold">Squad Manager</h1>    
+            <div class="login-card w-50 me-auto ms-auto" style="background-color: #1f2121">
+                <div class="mb-4 text-wrap text-white">
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
+                <div class="block">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                </div>
 
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
+                <div class="d-flex mt-4">
+                    <x-button class="w-50 form-control bg-secondary">
+                        {{ __('Email Password Reset Link') }}
+                    </x-button>
+                    <a class="underline text-sm text-gray-600 ms-4 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                        {{ __('Login Page') }}
+                    </a>
+                </div>
             </div>
         </form>
-    </x-authentication-card>
-</x-guest-layout>
+    </div>
+</main>
+
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.simple')
 
-@section('title', 'Login - Squad Manager')
+@section('title', 'Register - Squad Manager')
 
 @section('content')
 
@@ -10,27 +10,28 @@
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
+            <h1 class="text-center mb-4 font-weight-bold">Squad Manager</h1>    
+            <div class="login-card" style="background-color: #1f2121">
+                <h2 class="text-center mb-4 font-weight-bold text-white">Welcome</h2>
+                <div>
+                    <x-label for="name" class="text-white" value="{{ __('Name') }}" />
+                    <x-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                </div>
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="email" class="text-white" value="{{ __('Email') }}" />
+                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="password" class="text-white" value="{{ __('Password') }}" />
+                    <x-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
+                <div class="mt-4">
+                    <x-label for="password_confirmation" class="text-white" value="{{ __('Confirm Password') }}" />
+                    <x-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
                     <x-label for="terms">
@@ -48,14 +49,15 @@
                 </div>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="d-flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-button class="ms-4">
+                <x-button class="ms-4 form-control bg-secondary">
                     {{ __('Register') }}
                 </x-button>
+                </div>
             </div>
         </form>
     </div>
